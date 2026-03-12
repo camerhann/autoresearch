e18946231c5e97bcb1d679142469308491f600f2
Chris Cameron-Hann <ccameronhann@gmail.com>
2026-03-12 07:40:18 +0000

    exp: weighted 4-way voting (RF=3, HGBT=2, ET=2, Ada=1)

--- train.py
+++ train.py
@@ -97,6 +97,7 @@ ada = AdaBoostClassifier(
 model = VotingClassifier(
     estimators=[("rf", rf), ("hgbt", hgbt), ("et", et), ("ada", ada)],
     voting="soft",
+    weights=[3, 2, 2, 1],  # RF strongest, HGBT/ET mid, AdaBoost weakest
     n_jobs=-1,
 )
 
