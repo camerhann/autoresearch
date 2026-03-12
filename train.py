13da451484d0661955ebb323d6806386d7fd6ed4
Chris Cameron-Hann <ccameronhann@gmail.com>
2026-03-12 08:46:05 +0000

    Revert "exp: increase RF1 and ET to 800 trees (stabilize main contributors)"
    This reverts commit da6299eb0d064a9bb6d15b9f8556b3db6d2ee66c.

--- train.py
+++ train.py
@@ -61,7 +61,7 @@ print(f"Engineered features: {X_train.shape[1]} total")
 # ---------------------------------------------------------------------------
 
 rf1 = RandomForestClassifier(
-    n_estimators=800,
+    n_estimators=500,
     max_depth=20,
     min_samples_leaf=10,
     n_jobs=-1,
@@ -89,7 +89,7 @@ hgbt = HistGradientBoostingClassifier(
 )
 
 et = ExtraTreesClassifier(
-    n_estimators=800,
+    n_estimators=500,
     max_depth=20,
     min_samples_leaf=10,
     n_jobs=-1,
